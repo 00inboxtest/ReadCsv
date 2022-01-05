c34040e345449fa3803c826c63f1ad0c855b6907
00inboxtest <00.inbox.test@gmail.com>
2022-01-05 19:20:47 +0530

    20 push

--- main.py
+++ main.py
@@ -1,52 +1,57 @@
-from flask import Flask
-from flask_restful import Api, Resource, reqparse
+from flask import Flask, jsonify
+from flask_restful import reqparse
 import pandas as pd
 
 app = Flask(__name__)
-api = Api(app)
 
-class Users(Resource):
-    def get(self):
-        data = pd.read_csv('product_hierarchy.csv')
-        data = data.to_dict('records')
-        return {'data' : data}, 200
+@app.route('/getproduct', methods=['GET'])
+def getproduct_records():
+    data = pd.read_csv('product_hierarchy.csv',nrows=10)
+    data = data.to_dict('records')
+    return jsonify({'data' :data,'code':200})
 
-
-    def post(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('name', required=True)
-        parser.add_argument('age', required=True)
-        parser.add_argument('city', required=True)
-        args = parser.parse_args()
-
-        data = pd.read_csv('product_hierarchy.csv')
-
-        new_data = pd.DataFrame({
-            'name'      : [args['name']],
-            'age'       : [args['age']],
-            'city'      : [args['city']]
-        })
-
-        data = data.append(new_data, ignore_index = True)
-        data.to_csv('product_hierarchy.csv', index=False)
-        return {'data' : new_data.to_dict('records')}, 201
-
-    def delete(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('product_id', required=True)
-        args = parser.parse_args()
-
-        data = pd.read_csv('product_hierarchy.csv')
-
-        data = data[data['product_id'] != args['product_id']]
-
-        data.to_csv('product_hierarchy.csv', index=False)
-        return {'message' : 'Record deleted successfully.'}, 200
+@app.route('/getproduct_hierarchywise', methods=['POST'])
+def getproduct_store_records():
+    parser = reqparse.RequestParser()
+    parser.add_argument('hierarchy1_id', required=True)
+    args = parser.parse_args()
+    data = pd.read_csv('product_hierarchy.csv',nrows=20)        
+    data = data [(data ["hierarchy1_id"] == args['hierarchy1_id'])]
+    data = data.to_dict('records')
+    return jsonify({'data' :data,'code':200})
 
 
+@app.route('/getsaledata', methods=['GET'])
+def getsaledata_records():
+    data = pd.read_csv('sales.csv',nrows=10)
+    data = data.to_dict('records')
+    return jsonify({'data' :data,'code':200})
 
-# Add URL endpoints
-api.add_resource(Users, '/users')
+@app.route('/getsale_productdata', methods=['POST'])
+def getsaledata_product_records():
+    parser = reqparse.RequestParser()
+    parser.add_argument('product_id', required=True)
+    args = parser.parse_args()
+    data = pd.read_csv('sales.csv',nrows=20)        
+    data = data [(data ["product_id"] == args['product_id'])]
+    data = data.to_dict('records')
+    return jsonify({'data' :data,'code':200})
+
+@app.route('/getcitydata', methods=['GET'])
+def getcitydata_records():
+    data = pd.read_csv('store_cities.csv',nrows=10)
+    data = data.to_dict('records')
+    return jsonify({'data' :data,'code':200})
+
+@app.route('/getcity_storetypedata', methods=['POST'])
+def getcitydata_storetype_records():
+    parser = reqparse.RequestParser()
+    parser.add_argument('city_id', required=True)
+    args = parser.parse_args()
+    data = pd.read_csv('store_cities.csv',nrows=50)        
+    data = data [(data ["city_id"] == args['city_id'])]
+    data = data.to_dict('records')
+    return jsonify({'data' :data,'code':200})
 
 if __name__ == '__main__':
     app.run()
