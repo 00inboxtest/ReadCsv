095fd380539b4f94cea161e8222b8b4f2707bea8
00inboxtest <00.inbox.test@gmail.com>
2022-01-05 22:32:29 +0530

    code push

--- main.py
+++ main.py
@@ -47,11 +47,36 @@ def getcitydata_records():
 def getcitydata_storetype_records():
     parser = reqparse.RequestParser()
     parser.add_argument('city_id', required=True)
+    parser.add_argument('start_date', required=True)
+    parser.add_argument('end_date', required=True)
     args = parser.parse_args()
     data = pd.read_csv('store_cities.csv',nrows=50)        
     data = data [(data ["city_id"] == args['city_id'])]
     data = data.to_dict('records')
-    return jsonify({'data' :data,'code':200})
+    store_ids=[]
+    for x in data:
+        store_ids.append(x['store_id'])
+    # print(store_ids)
+
+    salesData = pd.read_csv('sales.csv',nrows=200)
+    # salesData.head()
+    print('store_ids',store_ids)
+
+    print('salesData',salesData)
+    # salesData['date']=salesData['date']
+    # salesData['date'] = pd.to_datetime(salesData['date'])
+    # newdf = salesData['date'] = args['start_date'] #& (salesData['date'] <= args['end_date'])
+    salesData[salesData.store_id.isin(store_ids)]
+    # newdf = salesData.loc[newdf]
+    # salesData = salesData [(salesData ["city_id"] == args['city_id'])]
+    salesData = salesData.to_dict('records')
+
+    return jsonify({
+            'code': 200,
+            'store_ids': store_ids,
+            # 'cityData' : data,
+            'salesData': salesData
+          })
 
 if __name__ == '__main__':
     app.run()
