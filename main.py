406bbfa3a4187a9b1b44ad3f7f2a729a8a288071
00inboxtest <00.inbox.test@gmail.com>
2022-01-06 15:02:08 +0530

    code push

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 from flask import Flask, jsonify
 from flask_restful import reqparse
 import pandas as pd
+import holidays
+import datetime
 
 app = Flask(__name__)
 
-@app.route('/getproduct', methods=['GET'])
-def getproduct_records():
-    data = pd.read_csv('product_hierarchy.csv')
-    data = data.to_dict('records')
-    return jsonify({'data' :data,'code':200})
+# @app.route('/getproduct', methods=['GET'])
+# def getproduct_records():
+#     data = pd.read_csv('product_hierarchy.csv')
+#     data = data.to_dict('records')
+#     return jsonify({'data' :data,'code':200})
 
+
+
+# Rquired parms for Post method
+# {
+#     "hierarchy1_id":"H00",
+#     "start_date":"2017-01-03",
+#     "end_date":"2017-01-03"
+# }
+
+
+# http://127.0.0.1:5000/getsale_productdata
 @app.route('/getproduct_hierarchywise', methods=['POST'])
 def getproduct_store_records():
     parser = reqparse.RequestParser()
@@ -40,26 +53,26 @@ def getproduct_store_records():
     salesData=salesData[product_filter]
 
     #sum of revenu and sales
-    sum_revenue=salesData['revenue'].sum()
-    sum_sales=salesData['sales'].sum()
+    total_revenue=salesData['revenue'].sum()
+    total_sales=salesData['sales'].sum()
 
     #output
     salesData = salesData.to_dict('records')
 
     return jsonify({
-            'total_revenue':sum_revenue,
-            'total_sales':sum_sales,
+            'total_revenue':total_revenue,
+            'total_sales':total_sales,
           })
 
 
-@app.route('/getsaledata', methods=['GET'])
-def getsaledata_records():
-    
-    #read sales data
-    data = pd.read_csv('sales.csv')
-    data = data.to_dict('records')
-    return jsonify({'data' :data,'code':200})
+# Rquired parms for Post method
+# {
+#     "product_id":"P0001",
+#     "start_date":"2017-01-02",
+#     "end_date":"2017-01-03"
+# }
 
+# http://127.0.0.1:5000/getsale_productdata
 @app.route('/getsale_productdata', methods=['POST'])
 def getsaledata_product_records():
     parser = reqparse.RequestParser()
@@ -73,9 +86,9 @@ def getsaledata_product_records():
     data = data [(data ["product_id"] == args['product_id'])]
     data = data.to_dict('records')
 
-    dimensions=0
+    total_dimensions=0
     for x in data:
-        dimensions=x['product_length']*x['product_width']*x['product_depth']
+        total_dimensions=x['product_length']*x['product_width']*x['product_depth']
 
     #read sales data
     salesData = pd.read_csv('sales.csv')
@@ -89,28 +102,30 @@ def getsaledata_product_records():
     salesData=salesData[product_filter]
 
     # sum of total sales
-    sum_sales=salesData['sales'].sum()
+    total_sales=salesData['sales'].sum()
 
     # calculate total volume
-    total_volume=dimensions*sum_sales
+    total_volume=total_dimensions*total_sales
     
     #output
     salesData = salesData.to_dict('records')
 
     return jsonify({
             'total_volume':total_volume,
-            'total_sales':sum_sales,
-            'total_dimensions':dimensions,
+            'total_sales':total_sales,
+            'total_dimensions':total_dimensions,
           })
 
 
-@app.route('/getcitydata', methods=['GET'])
-def getcitydata_records():
-    #read cities data
-    data = pd.read_csv('store_cities.csv')
-    data = data.to_dict('records')
-    return jsonify({'data' :data,'code':200})
 
+# Rquired parms for Post method
+# {
+#     "city_id":"C013",
+#     "start_date":"2017-01-03",
+#     "end_date":"2017-01-03"
+# }
+
+# http://127.0.0.1:5000/getcity_storetypedata
 @app.route('/getcity_storetypedata', methods=['POST'])
 def getcitydata_storetype_records():
     parser = reqparse.RequestParser()
@@ -140,20 +155,40 @@ def getcitydata_storetype_records():
     salesData=salesData[store_filter]
 
     #sum of revenu and sales
-    sum_revenue=salesData['revenue'].sum()
-    sum_sales=salesData['sales'].sum()
-    
-    # a_series = (salesData['sales'] != 0)
-    # new_df = salesData['sales'].loc[a_series]
+    total_revenue=salesData['revenue'].sum()
+    total_sales=salesData['sales'].sum()
 
     #output
     salesData = salesData.to_dict('records')
 
     return jsonify({
             'code': 200,
-            'total_revenue':sum_revenue,
-            'total_sales':sum_sales,
+            'total_revenue':total_revenue,
+            'total_sales':total_sales,
           })
+
+# http://127.0.0.1:5000/get_holidaysales
+@app.route('/get_holidaysales', methods=['GET'])
+def getholidays_records():
+    holiday_dates=[]
+    for date in holidays.Sweden(years=2018).items():
+        holiday_dates.append(str(date[0]))
+
+     #read sales data
+    salesData = pd.read_csv('sales.csv')
+    
+    #store filter
+    store_filter=salesData.date.isin(holiday_dates)
+    salesData=salesData[store_filter]
+
+    #sum of revenu and sales
+    total_revenue=salesData['revenue'].sum()
+    total_sales=salesData['sales'].sum()
+
+    return jsonify({
+        'total_revenue':total_revenue,
+        'total_sales':total_sales,
+    })
 
 if __name__ == '__main__':
     app.run()
